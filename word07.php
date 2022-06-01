334df7dda8033ed8eb759f3f926d892d48e52450
LongcayanaPatriciaMonick2D <patricialongcayana@gmail.com>
2022-06-01 13:01:47 +0800

    word 7 page added

---
+++ word07.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Floccinaucinihilipilification</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <style>
+    @import url('https://fonts.googleapis.com/css?family=Montserrat&display=swap');
+
+    body {
+        animation: fadeInPage ease 1.5s;
+        /* fade page in */
+        animation-iteration-count: 1;
+        /* fade page in */
+        animation-fill-mode: forwards;
+        /* fade page in */
+        background-color: lightseagreen;
+        font-family: 'Montserrat', sans-serif;
+        color: #fef9d5;
+        margin: 0;
+    }
+    </style>
+</head>
+
+<body>
+    <div class="position-absolute top-0 start-0">
+        <input class="btn btn-primary fw-bold" type="button" value="Back" onclick="history.back()">
+    </div><br><br><br>
+    <div class="p-3 text-center">
+        <h1 class="fw-bold">Floccinaucinihilipilification  </h1>
+    </div>
+    <div class="container ">
+        <div class="p-5 d-flex p-2 justify-content-center text-left">
+            <div class="row">
+
+                <?php
+                    echo "It  is the act of considering something to be worthless, as in I was outraged by the pawn shop owner's floccinaucinihilipilification. <br>";
+                    echo "It means the estimation of something as worthless.  <br>";
+                    echo "It is the estimation of something as valueless. <br>";             
+                     
+                ?>
+
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
+
+</html>

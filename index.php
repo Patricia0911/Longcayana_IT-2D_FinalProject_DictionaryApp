f65a92d6170c8bc02ae6dbcc854012b2e44e6d5a
LongcayanaPatriciaMonick2D <patricialongcayana@gmail.com>
2022-06-01 10:54:56 +0800

    Home Page

---
+++ index.php
@@ -0,0 +1,68 @@
+<!doctype html>
+<html lang="en" class="h-100">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="description" content="Web app using Php ">
+    <meta name="author" content="Patricia Monick Longcayana">
+    <meta name="application-name" content="TrishDictionary">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Home</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+
+
+    <!-- Custom styles for this template -->
+    <link href="style.css" rel="stylesheet">
+</head>
+
+<header class="mb-auto p-3">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top">TrishDictionary</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="index.php">HOME</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="riddle.php">Dictionary</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="about.php">ABOUT</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="contact.php">CONTACT</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+            
+</header>
+<header class="bg-dark bg-gradient text-white">
+            <div class="over-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+                <h1 class="fw-bolder">Welcome to TrishDictionary</h1>
+            </div>
+</header>
+<body class="background">
+
+    <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+
+        <main class="px-3">
+            <h1>“Dictionaries are like watches; the worst is better than none, and the best cannot be expected to go quite true.”</h1><br><br>
+            <p class="lead">― Samuel Johnson</p>
+        </main>
+        <main class="px-3">
+            <h1>“Dictionaries are the record of how people are already using the language, not providers of words for us to start using.”</h1><br><br>
+            <p class="lead">― Gretchen McCulloch</p>
+        </main>
+       
+    </div>
+
+</body>
+
+<footer class="py-3 bg-dark">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Patricia Monick Longcayana</p></div>
+    </footer>
+
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous">
+    </script>
+
+</html>
